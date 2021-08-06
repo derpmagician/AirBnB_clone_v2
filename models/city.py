38a41b847ef088e28f48103bef86790f7982ea85
derpmagician <renzin@gmail.com>
2021-08-06 13:35:26 -0500

    updated with possible changes as hashtag comments

--- models/city.py
+++ models/city.py
@@ -7,3 +7,25 @@ class City(BaseModel):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
+
+# Las lineas de abajo es lo que quiero reemplazar en vez de lo de arriba
+# (quitar los michi y borrar lo de arriba menos el sheabang)
+
+""" City Module for HBNB project """
+# from models.base_model import BaseModel, Base
+# from sqlalchemy import Column, String, ForeignKey
+# from sqlalchemy.orm import relationship
+
+
+# class City(BaseModel, Base):
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
+
+#    __tablename__ = 'cities'
+
+#    name = Column(String(128), nullable=False)
+#    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+#    places = relationship('Place', backref='cities')
