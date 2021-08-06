cadaf9ca4603fec113fd0415101dfe2f1bb54192
derpmagician <renzin@gmail.com>
2021-08-06 13:38:29 -0500

    updated with possible changes as hashtag comments

--- models/state.py
+++ models/state.py
@@ -6,3 +6,38 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
+
+# Las lineas de abajo es lo que quiero reemplazar en vez de lo de arriba
+# (quitar los michi y borrar lo de arriba menos el sheabang)
+
+"""This is the state class"""
+# from models.base_model import BaseModel, Base
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import Column, Integer, String, ForeignKey
+# from sqlalchemy.orm import relationship
+# from models.city import City
+# import models
+# from os import getenv
+
+
+# class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+#    __tablename__ = 'states'
+#    name = Column(String(128), nullable=False)
+#    cities = relationship("City", backref="state",
+#                          cascade="all, delete, delete-orphan")
+#    if getenv("HBNB_TYPE_STORAGE") != "db":
+#        @property
+#        def cities(self):
+            """
+            returns the list of City instances with state_id equals
+            to the current State.id
+            """
+#            list_city = []
+#            for city in models.storage.all(City).values():
+#                if city.state_id == self.id:
+#                    list_city.append(city)
+#            return list_city
