6b96fe2e6f59bdddc9cd05b3d945b128b60548cb
derpmagician <renzin@gmail.com>
2021-08-06 13:33:49 -0500

    updated with possible changes as hashtag comments

--- models/user.py
+++ models/user.py
@@ -9,3 +9,29 @@ class User(BaseModel):
     password = ''
     first_name = ''
     last_name = ''
+
+# Las lineas de abajo es lo que quiero reemplazar en vez de lo de arriba
+# (quitar los michi y borrar lo de arriba menos el sheabang)
+
+"""This module defines a class User"""
+# from models.base_model import BaseModel, Base
+# from sqlalchemy import Column, String
+# from sqlalchemy.orm import relationship
+
+# class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+
+#    __tablename__ = 'users'
+
+#    email = Column(String(128), nullable=False)
+#    password = Column(String(128), nullable=False)
+#    first_name = Column(String(128))
+#    last_name = Column(String(128))
+#    places = relationship('Place', backref='user')
+#    reviews = relationship('Review', backref='user')
