c07190cb74b223a86bcfb6d7da29c55e3e9d5e33
derpmagician <renzin@gmail.com>
2021-08-07 16:54:04 -0500

    [UPDATE] deleting IDENTIFIED BY command part

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,5 @@
 -- Creates the database hbnb_dev_db with specified paramenters
-CREATE DATABASE IF NOT EXISTS 'hbnb_test_db';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT PRIVILEGES ON 'performance_schema'.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-FLUSH PRIVILEGES
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
