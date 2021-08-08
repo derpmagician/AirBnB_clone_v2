e181329067f3ad1455d3894b707c892e1c641f45
Alex Rivera <alexriveracruz4@gmail.com>
2021-08-08 15:43:42 +0000

    [UPDATE] class attributes and DBStorage

--- models/state.py
+++ models/state.py
@@ -1,43 +1,34 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """ State class """
-    name = ""
-
-# Las lineas de abajo es lo que quiero reemplazar en vez de lo de arriba
-# (quitar los michi y borrar lo de arriba menos el sheabang)
-
 """This is the state class"""
-# from models.base_model import BaseModel, Base
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy import Column, Integer, String, ForeignKey
-# from sqlalchemy.orm import relationship
-# from models.city import City
-# import models
-# from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
+from os import getenv
 
 
-# class State(BaseModel, Base):
-#     """This is the class for State
-#     Attributes:
-#         name: input name
-#     """
-#     __tablename__ = 'states'
-#     name = Column(String(128), nullable=False)
-#     cities = relationship("City", backref="state",
-#                           cascade="all, delete, delete-orphan")
-#     if getenv("HBNB_TYPE_STORAGE") != "db":
-#         @property
-#         def cities(self):
-#             """
-#             returns the list of City instances with state_id equals
-#             to the current State.id
-#             """
-#             list_city = []
-#             for city in models.storage.all(City).values():
-#                 if city.state_id == self.id:
-#                     list_city.append(city)
-#             return list_city
+class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+    name: input name
+    """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """
+            returns the list of City instances with state_id equals
+            to the current State.id
+            """
+            list_city = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    list_city.append(city)
+            return list_city
