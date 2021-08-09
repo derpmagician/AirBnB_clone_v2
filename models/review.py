8f11cb9a5cbe895560262ef0d638597ac69a5cec
Alex Rivera <alexriveracruz4@gmail.com>
2021-08-09 23:40:30 +0000

    [UPDATE] class review

--- models/review.py
+++ models/review.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+"""This is the review class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """This is the class for Review
+    Attributes:
+    place_id: place id
+    user_id: user id
+    text: review description
+    """
+    __tablename__ = 'reviews'
 
-# Las lineas de abajo es lo que quiero reemplazar en vez de lo de arriba
-# (quitar los michi y borrar lo de arriba menos el sheabang)
-
-# """This is the review class"""
-# from models.base_model import BaseModel, Base
-# from sqlalchemy import Column, String, ForeignKey
-# from sqlalchemy.orm import relationship
-
-
-# class Review(BaseModel, Base):
-#     """This is the class for Review
-#     Attributes:
-#         place_id: place id
-#         user_id: user id
-#         text: review description
-#     """
-#     __tablename__ = 'reviews'
-
-#     text = Column(String(1024), nullable=False)
-#     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-#     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
