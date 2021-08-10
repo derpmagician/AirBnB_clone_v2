e41dde1f8b5470dd76eef417255ff2af521ce9ab
Alex Rivera <alexriveracruz4@gmail.com>
2021-08-10 21:25:11 +0000

    [ADD] test for save method

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -108,6 +108,24 @@ class TestFileStorage(unittest.TestCase):
 
         self.assertIs(self.storage.reload(), None)
 
+    def test_save(self):
+        """Test that save properly saves objects to file.json"""
+        storage = FileStorage()
+        new_dict = {}
+        for key, value in classes.items():
+            instance = value()
+            instance_key = instance.__class__.__name__ + "." + instance.id
+            new_dict[instance_key] = instance
+        save = FileStorage._FileStorage__objects
+        FileStorage._FileStorage__objects = new_dict
+        storage.save()
+        FileStorage._FileStorage__objects = save
+        for key, value in new_dict.items():
+            new_dict[key] = value.to_dict()
+        string = json.dumps(new_dict)
+        with open("file.json", "r") as f:
+            js = f.read()
+        self.assertEqual(json.loads(string), json.loads(js))
 
 if __name__ == "__main__":
     unittest.main()
