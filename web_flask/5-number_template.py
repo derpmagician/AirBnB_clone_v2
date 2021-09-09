9b3a508d0c4aa6ac7e7da5bc73c89fa9937b7e6e
derpmagician <renzin@gmail.com>
2021-09-09 16:09:13 -0500

    [ADD] file

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" flask """
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """print"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """print"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def ctext(text):
+    """print text"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pytext(text='is cool'):
+    """print text"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def nnumber(n):
+    """"Print number"""
+    if isinstance(n, int):
+        return '{} is a number'.format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def ntemplate(n):
+    """"Display a HTML page only if n is an intege"""
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run()
