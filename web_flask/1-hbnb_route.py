99cff5332aa6d42e5b59b2e1c24dd2e17c51ef0d
derpmagician <renzin@gmail.com>
2021-09-11 09:02:19 -0500

    [UPDATE] extra space

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-
-"""
-flask
-"""
-
+""" flask """
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def hello():
     """print"""
-    return ("Hello HBNB!")
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """no hellow"""
+    """print"""
     return ("HBNB")
 
 
