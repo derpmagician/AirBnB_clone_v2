943246567505431b58cc79710351238f77d02c50
derpmagician <renzin@gmail.com>
2021-09-11 12:11:49 -0500

    [UPDATE] methods

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,7 +15,7 @@ def close_storage(exception):
 
 @app.route('/states_list', strict_slashes=False)
 def states_list_all():
-    """ List States """
+    """List States"""
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
