4d50ee783c32eb58c72ad6d1b04a26f08b7fd39b
derpmagician <renzin@gmail.com>
2021-09-11 08:46:50 -0500

    [UPDATE] parenthesis

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ flask """
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,12 +9,12 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """print"""
-    return 'Hello HBNB!'
+    return ('Hello HBNB!')
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """print"""
-    return 'HBNB'
+    return ('HBNB')
 
 
 if __name__ == '__main__':
