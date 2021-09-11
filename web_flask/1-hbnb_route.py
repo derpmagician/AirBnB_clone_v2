393ef4ef84f7f58fcbfa433b34758ec76814d782
derpmagician <renzin@gmail.com>
2021-09-11 08:55:27 -0500

    [UPDATE] file

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,22 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 
 """
-flask
+starts a flask web application
 """
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def index():
-    """ print """
+    """ returns a message"""
     return ("Hello HBNB!")
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ print """
+    """new path"""
     return ("HBNB")
 
 
