2caf6f8f11033902e474fd0a63ddbf7c6b366501
derpmagician <renzin@gmail.com>
2021-09-11 14:56:18 -0500

    [ADD] file

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,70 +7,25 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """print"""
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """print"""
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def ctext(text):
-    """print text"""
-    return 'C {}'.format(text.replace('_', ' '))
-
-
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def pytext(text='is cool'):
-    """print text"""
-    return 'Python {}'.format(text.replace('_', ' '))
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def nnumber(n):
-    """"Print number"""
-    if isinstance(n, int):
-        return '{} is a number'.format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def ntemplate(n):
-    """"Display a HTML page only if n is an integer"""
-    if isinstance(n, int):
-        return render_template('5-number.html', n=n)
-
-
 @app.teardown_appcontext
-def close_storage(self):
+def close_storage(exception):
     """Remove the current SQLAlchemy Session."""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def h1():
-    """ List States """
-    states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states)
-
-
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """Display HTML with cities by states"""
-    states = storage.all(State).values()
-    cities = list()
+    def cities_by_states():
+        """List States by City"""
+        states = storage.all(State).values()
+        cities = list()
 
-    for state in states:
+        for state in states:
             for city in state.cities:
                 cities.append(city)
 
         return render_template('8-cities_by_states.html',
                                states=states, state_cities=cities)
+
 
 if __name__ == '__main__':
     app.run()
