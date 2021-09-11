d8d4492a87530691a5a6dca0b01eaeb749afdc60
derpmagician <renzin@gmail.com>
2021-09-11 12:03:09 -0500

    [UPDATE] methods

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,53 +7,14 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """print"""
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """print"""
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def ctext(text):
-    """print text"""
-    return 'C {}'.format(text.replace('_', ' '))
-
-
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def pytext(text='is cool'):
-    """print text"""
-    return 'Python {}'.format(text.replace('_', ' '))
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def nnumber(n):
-    """"Print number"""
-    if isinstance(n, int):
-        return '{} is a number'.format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def ntemplate(n):
-    """"Display a HTML page only if n is an integer"""
-    if isinstance(n, int):
-        return render_template('5-number.html', n=n)
-
-
 @app.teardown_appcontext
-def close_storage(self):
+def close_storage(exception):
     """Remove the current SQLAlchemy Session."""
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
-def h1():
+def states_list_all():
     """ List States """
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
